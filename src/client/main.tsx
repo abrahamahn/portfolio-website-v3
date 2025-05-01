8c08fb141d4910993c39a4836f7cf27ab95f0d76
abrahamahn <satmorningrain@gmail.com>
2025-05-01 21:29:32 +0900

    Add Google Analytics integration

--- src/client/main.tsx
+++ src/client/main.tsx
@@ -4,10 +4,16 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import App from "./App.tsx";
 import "./global.css";
+import { initGA } from "./utils/analytics";
+import RouteChangeTracker from "./utils/RouteChangeTracker";
+
+// Initialize Google Analytics
+initGA('G-MK7EC996CC');
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <BrowserRouter>
+      <RouteChangeTracker />
       <App />
     </BrowserRouter>
   </Provider>
