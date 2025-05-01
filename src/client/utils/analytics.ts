e739aef30293d5d740541f8d48e64e94aa7be06d
abrahamahn <satmorningrain@gmail.com>
2025-05-01 21:31:11 +0900

    Fix analytics module TypeScript errors

--- src/client/utils/analytics.ts
+++ src/client/utils/analytics.ts
@@ -1,1 +1,23 @@
- 
+import ReactGA from 'react-ga4';
+
+export const initGA = (measurementId: string): void => {
+  ReactGA.initialize(measurementId);
+};
+
+export const logPageView = (): void => {
+  ReactGA.send({ hitType: "pageview", page: window.location.pathname });
+};
+
+export const logEvent = (
+  category: string,
+  action: string,
+  label?: string,
+  value?: number
+): void => {
+  ReactGA.event({
+    category,
+    action,
+    label,
+    value
+  });
+}; 
